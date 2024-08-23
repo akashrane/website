529e3e71c545c225060ff5f3663b6c1dbde1f54c
Akash Rane <69100859+akashrane@users.noreply.github.com>
2024-08-23 01:11:36 -0400

    v1

---
+++ script.js
@@ -0,0 +1,6 @@
+function ToggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".akash-icon");
+    menu.classList.toggle("Open");
+    icon.classList.toggle("Open");
+}
